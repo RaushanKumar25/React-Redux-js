096c69c28667ce2fc3174e29651d81d41923bf8a
RaushanKumar25 <raushankumarbhardwaj4510@gmail.com>
2025-11-06 01:27:22 +0530

    added button date and input text from bootstrap

--- project/todo-app-version-one/src/App.jsx
+++ project/todo-app-version-one/src/App.jsx
@@ -1,10 +1,22 @@
-
-
 function App() {
-  return <center>placholder
-    <h1>TODO App</h1>
-  </center>
-  
+  return (
+    <center>
+      <h1>TODO App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Whats your TODO?" />
+          </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">Add</button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
