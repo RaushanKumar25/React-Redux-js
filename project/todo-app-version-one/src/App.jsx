1e0218309a3214b5e1c64baa218f2c941b6ef8b6
RaushanKumar25 <raushankumarbhardwaj4510@gmail.com>
2025-11-06 23:06:41 +0530

    added new component like TodoItem1 and AppName.jsx

--- project/todo-app-version-one/src/App.jsx
+++ project/todo-app-version-one/src/App.jsx
@@ -1,42 +1,21 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+
 function App() {
   return (
     <center>
-      <h1>TODO App</h1>
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder="Whats your TODO?" />
-          </div>
-          <div class="col-4">
-            <input type="date" />
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">Add</button>
-          </div>
-        </div>
+      <AppName />
+      <AddTodo />
+      <TodoItem1 />
 
-        <div class="row">
-          <div class="col-6">
-            Buy Groceries
-          </div>
-          <div class="col-4">
-            06/11/2025
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">Delete</button>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-6">
-            Complete Assignment
-          </div>
-          <div class="col-4">
-            06/11/2025
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">Delete</button>
-          </div>
+      <div className="row mt-3">
+        <div className="col-6">Complete Assignment</div>
+        <div className="col-4">06/11/2025</div>
+        <div className="col-2">
+          <button type="button" className="btn btn-danger">
+            Delete
+          </button>
         </div>
       </div>
     </center>
