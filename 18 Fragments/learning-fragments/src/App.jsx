3ed966ed4b2e3044c726796f393a8397276dcec7
RaushanKumar25 <raushankumarbhardwaj4510@gmail.com>
2025-11-12 13:51:35 +0530

    created a list for healthy food

--- 18 Fragments/learning-fragments/src/App.jsx
+++ 18 Fragments/learning-fragments/src/App.jsx
@@ -1,6 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 function App() {
-  return (
-    <div>Fragments</div>
+  return ( <div>
+    <h1>Healthy Food Items</h1>
+
+  <ul class="list-group">
+  <li class="list-group-item">An item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
+</div>
   )
 }
 
