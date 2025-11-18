458300ebdebcf0014fc0c47228d3629fd9d6ed79
RaushanKumar25 <raushankumarbhardwaj4510@gmail.com>
2025-11-19 02:05:59 +0530

    updated the readme file for css modules

--- 18 Fragments/learning-fragments/src/components/FoodItems.jsx
+++ 18 Fragments/learning-fragments/src/components/FoodItems.jsx
@@ -1,13 +1,15 @@
+import styles from "./FoodItems.module.css";
 const FoodItems = ({ items }) => {
   return (
     <ul className="list-group">
+      {" "}
       {items.map((item) => (
         <li key={item} className="list-group-item kg-item">
-          <span className = "kg-span">{item}</span>
+          {" "}
+          <span className="kg-span">{item}</span>{" "}
         </li>
-      ))}
+      ))}{" "}
     </ul>
   );
 };
-
 export default FoodItems;
